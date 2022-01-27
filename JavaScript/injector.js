81c94b978aedbb66a281a3f69cc2277cc7d4f0cc
Aaron Wile <itsbeenawile@gmail.com>
2022-01-27 12:16:18 -0500

    Added injector changes missed in repo migration.

--- JavaScript/injector.js
+++ JavaScript/injector.js
@@ -19,8 +19,10 @@ var scriptText = `
 
 var funcLogger = {}; //funcLogger holds the interception functions
 funcLogger.funcsCalled = []; //funcsCalled will store a list of D3 functions called by the page.
+funcLogger.argList = [];
 var alreadyFired = false; //to ensure D3 source code is only fired once.
 var iframeList = []; //Cannot detect code inside of iframes, we'll let the Parser know they're here
+var needArgs = ["select", "selectAll", "attr", "svg", "csv", "append"];
 
 //-------------------------------------------------------------------------------------------------
 // Functions
@@ -40,9 +42,41 @@ funcLogger.replace = function(old_func, func_name)
         if(!funcLogger.funcsCalled.includes(func_name))
             funcLogger.funcsCalled.push(func_name);
 
+        // Main idea:
+        if(needArgs.includes(func_name) && arguments)   // if the needArgs array contains the
+        {
+            var argString = func_name + "(";
+            var count = 0;
+            for(let arg of arguments)
+            {
+                if(count > 0)
+                    argString += ", ";
+
+                 argString += arg.toString();
+                 count++;
+            }
+            argString += ")";
+            funcLogger.argList.push(argString);
+        }
+
         //Return the old function with this extra snippet of code appended.
         return old_func.apply(this, arguments);
     }
+}
+
+function interceptNative(){
+    if(document){
+        for(var i = 0; i < needArgs.length; i++){
+            var name = needArgs[i];
+            var func = document[name];
+            console.log(name);
+            if(typeof func == 'function'){
+                document[name] = funcLogger.replace(func, name); //...replace it.
+                console.log("is function")
+            }
+        }
+    }
+
 }
 
 //InterceptD3() checks whether D3 source code has loaded on the page. If it has, it uses
@@ -61,14 +95,17 @@ function interceptD3()
         //Set internal fields to avoid a second interception, disable the mutation observer.
         alreadyFired = true;
         mo.disconnect();
-        console.log("Expert Goggles: Interceptor Fired.")
+        console.log("Expert Goggles: Interceptor Fired. D3.")
 
         //For all fields in window.d3...
         for(var name in window.d3)
         {
+
             var func = window.d3[name];
-            if(typeof func == "function") //...if that field is a function...
+            if(typeof func == "function"){ //...if that field is a function...
                 window.d3[name] = funcLogger.replace(func, name); //...replace it.
+                console.log(name)
+            }
             else //...if that field is not a function...
                 for(var subName in window.d3[name])
                 {
@@ -79,6 +116,7 @@ function interceptD3()
                 }
         }
     }
+
 }
 
 //inspectMutations() is the callback function for our mutation observer. It takes a list
@@ -96,6 +134,7 @@ function inspectMutations(mutations)
         {
             if(newNode.tagName == "SCRIPT")
                 interceptD3();
+                interceptNative();
         }
         catch(err){}//Thousands of mutations occur, we can't handle errors w/o slowing the page.
     }
@@ -109,8 +148,13 @@ function sendToParser()
     //Generate a message with the necessary info
     var message = {};
     message.funcList = funcLogger.funcsCalled;
+    message.argList = funcLogger.argList;
     message.sender = "ExpertGogglesInterceptor";
     message.iframeList = iframeList;
+
+    //Log Info Out
+    console.log(message.funcList);
+    console.log(message.argList);
 
     //Since this script is injected, we have to use window.postMessage
     try{window.postMessage(message, "*");}
@@ -122,6 +166,9 @@ function sendToParser()
 //-------------------------------------------------------------------------------------------------
 
 //The interceptor is appended to document before anything else loads.
+
+interceptjQuery();
+
 //Create a mutation observer to watch for changes to the DOM. inspectMutations() is the callback.
 var mo = new MutationObserver(inspectMutations);
 mo.observe(document, {subtree: true, childList: true});
@@ -138,6 +185,7 @@ document.addEventListener("DOMContentLoaded", function()
 //D3 code is manipulating elements. 1.2 seconds seems to be a reliable timer.
 setTimeout( function() {sendToParser();}, 1200);
 `;
+
 
 //-------------------------------------------------------------------------------------------------
 // End of Interceptor Code --> Injector Code
